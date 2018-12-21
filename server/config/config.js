5d2ec3810d9cbe4d74fec3a3533957c3706c8a17
Lucca <lucca86@gmail.com>
2018-12-21 17:00:02 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,24 @@
 //==============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==============================
+// ENTORNO
+//==============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==============================
+// BASE DE DATOS
+// Conexión a MongoDB de MLAB
+//==============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:abcd1234@ds141704.mlab.com:41704/cafe';
+}
+
+process.env.URLDB = urlDB;
